2b76052858eb56952ed4a1037ac2200c7e738bff
Tony Cioara <tonyangelo9707@gmail.com>
2018-10-30 12:28:51 -0700

    Added updateAt and createdAt to post model

--- models/post.js
+++ models/post.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-    title: { type: String, required: true },
-    url: { type: String, required: true },
-    summary: { type: String, required: true },
+const PostSchema = new Schema({
+    createdAt: {type: Date},
+    updatedAt: {type: Date},
+    title: {type: String, required: true},
+    url: {type: String, required: true},
+    summary: {type: String, required: true}
 });
 
-module.exports = mongoose.model('Post', postSchema);
+PostSchema.pre('save', function(next) {
+    // SET createAt and updateAT
+    const now = new Date();
+    this.updatedAt = now;
+
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('Post', PostSchema);
