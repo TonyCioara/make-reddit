e8119828d9178f5e7cdc6c08d5428411a189a04d
Tony Cioara <tonyangelo9707@gmail.com>
2018-10-30 18:18:50 -0700

    Added tests for current routes -INCOMPLETE

---
+++ test/posts.js
@@ -0,0 +1,25 @@
+
+const Post = require('../models/post.js');
+
+describe('Posts', () => {
+    it('should create with valid attributes at POST /posts/new', done => {
+
+        const newPost = { title: "post title", url: "https://www.google.com", summary: "post summary" };
+
+        Post.findOneAndRemove(newPost, () => {
+            Post.find((err, posts) => {
+                var postCount = posts.length;
+                chai
+                .request('localhost:3000')
+                .post('/posts/new', newPost)
+                .end((err, res) => {
+                    Post.find((err, posts) => {
+                        postCount.should.be.equal(posts.length - 1);
+                        res.should.be.equal(200);
+                        done();
+                    });
+                });
+            });
+        })
+    });
+});
